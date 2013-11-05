7ece3644844dd6d7df07ac2b3c59f796023ccc6c
saissaoui <sofiene.aissaoui@gmail.com>
2013-11-05 11:47:15 +0100

    Update GpsDTO.java

--- src/main/java/fr/soat/hibernategwt/shared/model/GpsDTO.java
+++ src/main/java/fr/soat/hibernategwt/shared/model/GpsDTO.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public class GpsDTO implements Serializable {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private int idGps;
 	private String nom;
